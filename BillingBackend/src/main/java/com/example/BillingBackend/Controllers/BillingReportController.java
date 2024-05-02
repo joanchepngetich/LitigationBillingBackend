54ba0302084147a305c04ff77e3db46f77ff032a
joanchepngetich <joanchepngetich01@gmail.com>
2024-05-02 09:14:58 +0300

    Jasper code updated

--- BillingBackend/src/main/java/com/example/BillingBackend/Controllers/BillingReportController.java
+++ BillingBackend/src/main/java/com/example/BillingBackend/Controllers/BillingReportController.java
@@ -1,36 +1,57 @@
-package com.example.BillingBackend.Controllers;
+package com.LDLS.Litigation.Project.BillingModule.Controllers;
 
-import com.example.BillingBackend.Entities.ExpenseTracking;
-import com.example.BillingBackend.Entities.InvoiceReports;
-import com.example.BillingBackend.Services.BillingReportService;
+import com.LDLS.Litigation.Project.BillingModule.Repositories.InvoiceReportsRepository;
+import net.sf.jasperreports.engine.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.*;
+
 
 @RestController
+@RequestMapping("/api/v1/reports")
 public class BillingReportController {
 
-    private final BillingReportService billingService;
-
     @Autowired
-    public BillingReportController(BillingReportService billingService) {
-        this.billingService = billingService;
-    }
+    InvoiceReportsRepository invoiceReportsRepository;
 
-    @GetMapping(value = "/billing/reports", produces = "application/json")
-    public ResponseEntity<Map<String, List<?>>> generateBillingReport() {
-        List<InvoiceReports> invoicesReports = billingService.getAllInvoicesReports();
-        List<ExpenseTracking> expenses = billingService.getAllExpenses();
+    @Value("${others.filesPath}")
+    private String filesPath;
 
-        Map<String, List<?>> report = new HashMap<>();
-        report.put("invoicesReports", invoicesReports);
-        report.put("expenses", expenses);
+    @Value("${others.logo}")
+    private String logo;
 
-        return ResponseEntity.ok(report);
+    @Value("${spring.datasource.url}")
+    private String db;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+
+    @GetMapping("/Invoices Report")
+    public ResponseEntity<ByteArrayResource> InvoicesReports() throws FileNotFoundException, JRException, SQLException {
+        Connection connection = DriverManager.getConnection(this.db, this.username, this.password);
+        ClassLoader classLoader = getClass().getClassLoader();
+        JasperReport compileReport = JasperCompileManager.compileReport(classLoader.getResourceAsStream("templates/InvoiceReport.jrxml"));
+        Map<String, Object> parameter = new HashMap<>();
+//        parameter.put("churchId", id);
+//        parameter.put("logo", logo);
+        JasperPrint report = JasperFillManager.fillReport(compileReport, parameter, connection);
+        byte[] data = JasperExportManager.exportReportToPdf(report);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Invoices_Reports.pdf");
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        ByteArrayResource byteArrayResource = new ByteArrayResource(data);
+        return ResponseEntity.ok().headers(headers).body(byteArrayResource);
     }
 }
--- BillingBackend/src/main/java/com/example/BillingBackend/Repositories/BillingReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BillingBackend.Repositories;
-
-import com.example.BillingBackend.Entities.BillingReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingReportRepository extends JpaRepository<BillingReport, Long> {
-}
